7bb90affe6de5c5e3e7bf01b6c09a298409c57e7
lavisar <sarlavi6969@gmail.com>
2024-03-18 23:10:55 +0700

    Insert Rectangle feat

--- components/room.tsx
+++ components/room.tsx
@@ -2,8 +2,10 @@
 
 import { ReactNode } from 'react';
 import { ClientSideSuspense } from '@liveblocks/react';
+import { LiveMap, LiveObject, LiveList } from '@liveblocks/client';
 
 import { RoomProvider } from '@/liveblocks.config';
+import { Layer } from '@/types/canvas';
 
 interface RoomProps {
 	children: ReactNode;
@@ -13,7 +15,14 @@ interface RoomProps {
 
 export const Room = ({ children, roomId, fallback }: RoomProps) => {
 	return (
-		<RoomProvider id={roomId} initialPresence={{ cursor: null }}>
+		<RoomProvider
+			id={roomId}
+			initialPresence={{ cursor: null, selection: [] }}
+			initialStorage={{
+				layers: new LiveMap<string, LiveObject<Layer>>(),
+				layerIds: new LiveList(),
+			}}
+		>
 			<ClientSideSuspense fallback={fallback}>
 				{() => children}
 			</ClientSideSuspense>
