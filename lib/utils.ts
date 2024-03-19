43dcf570275481356076cc6c26d7001a168d6a7c
lavisar <sarlavi6969@gmail.com>
2024-03-19 12:52:11 +0700

    Fix resizing bug

--- lib/utils.ts
+++ lib/utils.ts
@@ -52,25 +52,25 @@ export function resizeBounds(bounds: XYHW, corner: Side, point: Point): XYHW {
 		height: bounds.height,
 	};
 
-	if ((corner && Side.Left) === Side.Left) {
-		result.x = Math.min(point.x, bounds.x + bounds.width);
-		result.width = Math.abs(bounds.x + bounds.width - point.x);
-	}
+	if ((corner & Side.Left) === Side.Left) {
+    result.x = Math.min(point.x, bounds.x + bounds.width);
+    result.width = Math.abs(bounds.x + bounds.width - point.x);
+  }
 
-	if ((corner && Side.Right) === Side.Right) {
-		result.x = Math.min(point.x, bounds.x);
-		result.width = Math.abs(point.x - bounds.x);
-	}
+  if ((corner & Side.Right) === Side.Right) {
+    result.x = Math.min(point.x, bounds.x);
+    result.width = Math.abs(point.x - bounds.x);
+  }
 
-	if ((corner && Side.Top) === Side.Top) {
-		result.y = Math.min(point.y, bounds.y, bounds.height);
-		result.height = Math.abs(bounds.y + bounds.height - point.y);
-	}
+  if ((corner & Side.Top) === Side.Top) {
+    result.y = Math.min(point.y, bounds.y + bounds.height);
+    result.height = Math.abs(bounds.y + bounds.height - point.y);
+  }
 
-	if ((corner && Side.Bottom) === Side.Bottom) {
-		result.y = Math.min(point.y, bounds.y);
-		result.height = Math.abs(point.y - bounds.y);
-	}
+  if ((corner & Side.Bottom) === Side.Bottom) {
+    result.y = Math.min(point.y, bounds.y);
+    result.height = Math.abs(point.y - bounds.y);
+  }
 
 	return result;
 }
